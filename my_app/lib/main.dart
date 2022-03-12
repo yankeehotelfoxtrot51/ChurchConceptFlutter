fa1a04d6f0da6e94be6e08056352f63933d2f8b4
Luis E. Bobadilla Dias <superluis5@gmail.com>
2022-03-12 17:58:24 -0300

    gif carrusel working in middle tab

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -30,7 +30,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
+  static List<Widget> _widgetOptions = <Widget>[
     Text(
       'Church Online',
       style: optionStyle,
@@ -39,10 +39,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
       'I\'m New',
       style: optionStyle,
     ),
-    Text(
-      'Listen\\Watch',
-      style: optionStyle,
-    ),
+    FullscreenSliderDemo(),
     Text(
       'Giving',
       style: optionStyle,
