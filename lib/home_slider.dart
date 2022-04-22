175d1fcead916f8d484540ced803eb55586bde46
yankeehotelfoxtrot51 <tbgrilliot@gmail.com>
2022-04-22 00:20:23 -0500

    init

---
+++ lib/home_slider.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+List<Map> slides = [
+  {
+    'id': 0,
+    'background': "assets/lando-calrissian-dance.gif",
+    'date': 'April 15wdaf',
+    'title': 'Ressurection: Part 3'
+  },
+  {
+    'id': 1,
+    'background': "assets/Lando-Leia.gif",
+    'date': 'Febuary 6 ',
+    'title': 'Terminator: Ressurection'
+  },
+  {
+    'id': 2,
+    'background': "assets/lando-return.gif",
+    'date': 'March 22',
+    'title': 'We Love Jesus'
+  },
+];
+
+List<String> imagePaths = [
+  "assets/lando-calrissian-dance.gif",
+  "assets/Lando-Leia.gif",
+  "assets/lando-return.gif",
+];
+
+class FullscreenSliderDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Builder(
+        builder: (context) {
+          final double _fullHeight = MediaQuery.of(context).size.height;
+          return CarouselSlider(
+            options: CarouselOptions(
+              height: _fullHeight,
+              viewportFraction: 1.0,
+              enlargeCenterPage: false,
+              enableInfiniteScroll: false,
+              autoPlay: true,
+            ),
+            items: slides
+                .map((item) => Container(
+                      padding: EdgeInsets.only(top: 500),
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage(item['background']),
+                              fit: BoxFit.cover,
+                              colorFilter: ColorFilter.mode(
+                                Colors.black54,
+                                BlendMode.darken,
+                              ))),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            flex: 2,
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Icon(
+                                Icons.share,
+                                color: Colors.white,
+                                size: 30,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 8,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                SvgPicture.asset('assets/icons/LoGo.svg'),
+                                Text(item['date'],
+                                    style: TextStyle(
+                                        color: Colors.white54,
+                                        fontWeight: FontWeight.w300,
+                                        fontSize: 18.0)),
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(0, 0, 0, 6.0),
+                                  child: Text(item['title'],
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w800,
+                                        fontSize: 24.0,
+                                      )),
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    ElevatedButton(
+                                        onPressed: null,
+                                        child: Row(children: [
+                                          Text('Experience'),
+                                          Icon(Icons.play_arrow_outlined)
+                                        ]),
+                                        style: ButtonStyle(
+                                            backgroundColor:
+                                                MaterialStateProperty.all(
+                                                    Colors.white),
+                                            foregroundColor:
+                                                MaterialStateProperty.all(
+                                                    Colors.black87),
+                                            padding: MaterialStateProperty.all(
+                                                EdgeInsets.symmetric(
+                                                    vertical: 0.5,
+                                                    horizontal: 15.0)),
+                                            minimumSize:
+                                                MaterialStateProperty.all(
+                                                    Size.zero)))
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: slides.map((value) {
+                                    int index = slides.indexOf(value);
+                                    return Container(
+                                        width: 8.0,
+                                        height: 8.0,
+                                        margin: EdgeInsets.symmetric(
+                                            vertical: 10.0, horizontal: 2.0),
+                                        decoration: BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            border: Border.all(
+                                                width: 1.2,
+                                                color: Colors.white)),
+                                        child: item['id'] == index
+                                            ? Container(
+                                                decoration: BoxDecoration(
+                                                border: Border.all(
+                                                    width: 1.2,
+                                                    color: Color.fromARGB(
+                                                        255, 120, 120, 120)),
+                                                shape: BoxShape.circle,
+                                                color: Color.fromARGB(
+                                                    255, 220, 220, 220),
+                                              ))
+                                            : null);
+                                  }).toList(),
+                                )
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Icon(
+                                Icons.favorite_border,
+                                color: Colors.white,
+                                size: 30,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ))
+                .toList(),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'home_slider.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+void main() => runApp(const MyApp());
+
+final themeMode = ValueNotifier(2);
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // static const String _title = 'Word of Life';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // title: _title,
+      home: MyStatefulWidget(),
+      theme: ThemeData(fontFamily: 'Lato'),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Church Online',
+      style: optionStyle,
+    ),
+    Text(
+      'I\'m New',
+      style: optionStyle,
+    ),
+    FullscreenSliderDemo(),
+    Text(
+      'Giving',
+      style: optionStyle,
+    ),
+    Text(
+      'Contact Us',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      extendBody: true,
+      appBar: AppBar(
+        // centerTitle: true,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SvgPicture.asset('assets/icons/LoGo.svg'),
+            Text(
+              'WORD OF LIFE CARLSBAD',
+              style: TextStyle(
+                  fontSize: 20, fontWeight: FontWeight.w900, letterSpacing: 1),
+            ),
+            IconButton(
+              constraints: BoxConstraints(),
+              padding: EdgeInsets.zero,
+              onPressed: null,
+              icon: SvgPicture.asset('assets/icons/LoGo.svg'),
+            )
+          ],
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        // elevation: 0, //getting rid of this leaves a shadow...
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.church),
+              label: 'Church Online',
+              backgroundColor: Colors.transparent),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.new_releases),
+              label: 'I\'m New',
+              backgroundColor: Colors.transparent),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Listen\\Watch',
+              backgroundColor: Colors.transparent),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.attach_money),
+              label: 'Giving',
+              backgroundColor: Colors.transparent),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.contact_mail),
+              label: 'Contact Us',
+              backgroundColor: Colors.transparent),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
